6d65e9448bc1718f217b1990058eed45e19510eb
Djoumoi Abdul <abdul_djoumoi@carrefour.com>
2023-12-01 15:24:28 +0100

    contract code generator

---
+++ bootstrap/src/main/java/com/example/karatemsdemo/KarateMsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.karatemsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarateMsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarateMsDemoApplication.class, args);
+    }
+
+}
